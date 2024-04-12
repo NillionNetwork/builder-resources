9ebdda82eee17e76a3397804952edf8b8d78b466
Nicholas Wehr <github@biochx.com>
2024-04-11 20:05:23 -0700

    chore: syncing lastest example programs from HEAD mainline; some of these might not be compatible with this sdk version

---
+++ resources/programs/multiplication_array_of_particles.py
@@ -0,0 +1,15 @@
+from nada_dsl import *
+
+
+def nada_main():
+    party1 = Party(name="Party1")
+    a = Array(SecretInteger(Input(name="a", party=party1)), size=3)
+    b = Array(SecretInteger(Input(name="b", party=party1)), size=3)
+
+    @nada_fn
+    def mul(left: SecretInteger, right: SecretInteger) -> SecretInteger:
+        return left * right
+
+    result = a.zip(b).map(mul)
+
+    return [Output(result, "my_output", party1)]
